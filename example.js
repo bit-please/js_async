986d05bb7b319a9ec0f17614dd81e5ff84e1d6f7
danizaghian <dani.zaghian@gmail.com>
2019-06-06 10:56:37 -0700

    adds nested callback solution

--- example.js
+++ example.js
@@ -1,4 +1,21 @@
+// setTimeout(function() {
+// 	console.log("First task done!");
+// }, 2000);
+
+// setTimeout(function() {
+// 	console.log("Second task done!");
+// }, 4000);
+
+// setTimeout(function() {
+// 	console.log("Third task done!");
+// }, 6000);
+
 setTimeout(function() {
 	console.log("First task done!");
+	setTimeout(function() {
+		console.log("Second task done!");
+		setTimeout(function() {
+			console.log("Third task done!");
+		}, 2000);
+	}, 2000);
 }, 2000);
-console.log("Second task done!");
--- example.rb
+++ example.rb
@@ -1,3 +1,8 @@
 sleep 2
 puts "First task done!"
+
+sleep 2
 puts "Second task done!"
+
+sleep 2
+puts "Third task done!"
